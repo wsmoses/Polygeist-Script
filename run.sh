61ad1003e85d9dbcc0b9133a7626adb56ff05721
Ubuntu <ubuntu@ip-172-31-15-98.ec2.internal>
2021-04-10 15:51:57 +0000

    update script

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-export PATH=$HOME/mlir-gpu/build/bin:$PATH
-export PATH=$HOME/pluto:$PATH
-export C_INCLUDE_PATH=$home/mlir-gpu/build/projects/openmp/runtime/src
+export PATH=$HOME/mlir-gpu/build/bin:$HOME/pluto:$HOME/polymer/build/bin:$PATH
+
+export C_INCLUDE_PATH=$HOME/mlir-gpu/build/projects/openmp/runtime/src
+export LD_LIBRARY_PATH=/home/ubuntu/polymer/build/pluto/lib/:/home/ubuntu/mlir-gpu/build/lib
 
 stdinclude=$HOME/mlir-gpu/llvm/../clang/lib/Headers
 
@@ -26,14 +27,17 @@ CFLAGS="-I $BASE/utilities -I $stdinclude -D POLYBENCH_TIME -D POLYBENCH_NO_FLUS
 
 #dirList="linear-algebra/blas"
 
-TOOLS="clangsing clang mlir-clang polly pollypar pluto plutopar polymer polymerpar"
+TOOLS="clang mlir-clang polly pollypar pluto plutopar polymer polymerpar"
 
 function run()
-{
-  
+{ 
   TOOL="$1"
   TEST="$2"
   OUT=$TEST.$TOOL.ll
+
+
+  echo "RUN: $TOOL with $TEST in $(pwd)"
+
   case $TOOL in
 
     clangsing)
@@ -49,16 +53,11 @@ function run()
       ;;
 
     polly)
-      clang $CFLAGS -O3 -S -emit-llvm $TEST.c -o $OUT \ 
-        -mllvm -polly -mllvm -polly-pattern-matching-based-opts=false -mllvm -polly-vectorizer=none
+      clang $CFLAGS -O3 -S -emit-llvm $TEST.c -o $OUT -mllvm -polly -mllvm -polly-pattern-matching-based-opts=false -mllvm -polly-vectorizer=none
       ;;
 
     pollypar)
-      clang $CFLAGS -O3 -S -emit-llvm $TEST.c -o $OUT \
-	      -mllvm -polly -mllvm -polly-pattern-matching-based-opts=false \
-	      -mllvm -polly-vectorizer=none -mllvm -polly-parallel -mllvm \
-	      -polly-parallel-force -mllvm -polly-omp-backend=LLVM \
-	      -mllvm -polly-scheduling=dynamic -mllvm -polly-scheduling-chunksize=4
+      clang $CFLAGS -O3 -S -emit-llvm $TEST.c -o $OUT -mllvm -polly -mllvm -polly-pattern-matching-based-opts=false -mllvm -polly-vectorizer=none -mllvm -polly-parallel -mllvm -polly-parallel-force -mllvm -polly-omp-backend=LLVM -mllvm -polly-scheduling=dynamic -mllvm -polly-scheduling-chunksize=4
       ;;
 
     pluto)
@@ -73,7 +72,7 @@ function run()
       ;;
 
     plutopar)
-      if [[ $subDir == "adi" ]]
+      if [[ $2 == "adi" ]]
       then
         RESULT="$TOOL:nan"
         return
@@ -83,12 +82,9 @@ function run()
       ;;
 
     polymer)
-      if [[ $subDir == "adi" ]]
-      then
-        RESULT="$TOOL:nan"
-        return
-      fi
+
       mlir-clang $CFLAGS $TEST.c -o $TEST.$TOOL.in.mlir
+
       polymer-opt -reg2mem \
       -insert-redundant-load \
       -extract-scop-stmt \
@@ -101,29 +97,17 @@ function run()
       ;;
 
     polymerpar)
-      if [[ $subDir == "adi" ]]
-      then
-        RESULT="$TOOL:nan"
-        return
-      fi
-      #TODO add parallelism flag
+      
+      
       mlir-clang $CFLAGS $TEST.c -o $TEST.$TOOL.in.mlir
+      
       polymer-opt --demote-loop-reduction \
            --extract-scop-stmt \
            --pluto-opt='parallelize=1' \
            --inline \
            --canonicalize $TEST.$TOOL.in.mlir 2>/dev/null > $TEST.$TOOL.out.mlir
 
-      mlir-opt --mem2reg \
-           --detect-reduction \
-           --mem2reg \
-           --canonicalize \
-           --affine-parallelize \
-           --lower-affine \
-           --convert-scf-to-openmp \
-           --convert-scf-to-std \
-           --convert-openmp-to-llvm  $TEST.$TOOL.out.mlir |\ 
-	mlir-translate -mlir-to-llvmir > $OUT
+      mlir-opt -mem2reg -detect-reduction -mem2reg -canonicalize -affine-parallelize -lower-affine -convert-scf-to-openmp -convert-scf-to-std -convert-openmp-to-llvm $TEST.$TOOL.out.mlir | mlir-translate -mlir-to-llvmir > $OUT
       ;;
 
     *)
@@ -131,12 +115,12 @@ function run()
       exit 1
       ;;
   esac
-  # TODO
-  if [[ $1 == "clang" || $1 == "polly" || $1 == "pluto" || $1 == "mlir-clang" ]] 
+  
+  if [[ $1 == "clang" || $1 == "polly" || $1 == "pluto" || $1 == "mlir-clang" || $1 == "polymer" ]] 
   then 
-  	clang $BASE/utilities/polybench.c -O3 -march=native $OUT -o $TEST.$TOOL.exe -lm -D POLYBENCH_NO_FLUSH_CACHE -D EXTRALARGE_DATASET
+  	clang $BASE/utilities/polybench.c -O3 -march=native $OUT -o $TEST.$TOOL.exe -lm -D POLYBENCH_TIME  -D POLYBENCH_NO_FLUSH_CACHE -D EXTRALARGE_DATASET
   else 
-	clang $BASE/utilities/polybench.c -O3 -march=native $OUT -o $TEST.$TOOL.exe -lm -fopenmp -D POLYBENCH_NO_FLUSH_CACHE -D EXTRALARGE_DATASET
+	clang $BASE/utilities/polybench.c -O3 -march=native $OUT -o $TEST.$TOOL.exe -lm -fopenmp -D POLYBENCH_TIME -D POLYBENCH_NO_FLUSH_CACHE -D EXTRALARGE_DATASET
   fi	
 }
 
@@ -145,10 +129,17 @@ for dir in $dirList; do
   for subDir in `ls`; do
     cd $subDir 
     
-    echo $(pwd)
-    #for tool in $TOOLS; do
-      run "pollypar" $subDir
-    #done 
+    #echo $(pwd)
+    #for t in $TOOLS; do
+    #  run $t $subDir
+    #done
+
+    for i in 1 2 3 4 5; do
+      for t in $TOOLS; do
+        time=$(taskset -c 1-8 numactl -i all ./$subDir.$t.exe)
+	echo $t:$subDir:$time
+      done
+    done 
 
     cd ../
   done
